647b2c9ae5c900386d062d54b447aa4b734e35ba
Mohammad Heydari <Hmohammad2520@gmail.com>
2026-02-18 18:17:46 +0330

    Session 2

---
+++ session_2.py
@@ -0,0 +1,49 @@
+## Varible Types
+
+# Hichi
+hichi = None
+
+# Moteghayer Haye Addadi
+int()
+float()
+bool()
+
+## NamGozari
+# 1. Ba addad shoro nemishan
+# 2. Bara kalamat az _ estefade shvad
+# 3. az in charecter ha nemitavan estefade kard !@#$%^&*()=+.,/|\
+# 4. Case Sensetive
+
+## Varible name --> snake_case
+# Ye zabon bashe
+
+total_student_numbers = 10
+pi = 3.14
+pi = 3.14159265359
+debt = -10_000_000_000
+is_currect = True
+
+
+## String
+str()
+bytes()
+
+first_name = 'Mohammad'
+middle_name = ''
+last_name = "Heydari"
+phone_number = '09195613940'
+national_code = '5560555258'
+
+## Array
+# 1 Bakhsh
+list()
+tuple()
+set()
+
+# 2 Bakhsh
+dict()
+
+students = ['Mohammad Heydari', 'Taha Ardavan']
+x = [1, True, False, None, '', 1.0]
+
+print(list('Mohammad'))
